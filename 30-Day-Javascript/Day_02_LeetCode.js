075a2bf7c3c89b38978a1ba4f16c889299359c3f
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-25 20:23:54 +0545

    Day 2

--- 30-Day-Javascript/Day_02_LeetCode.js
+++ 30-Day-Javascript/Day_02_LeetCode.js
@@ -6,7 +6,27 @@ const age = 20;
 
 let isValid = true;
 
+//username validation
 if (Username === "") {
   console.log("Username cannot be empty");
   isValid = false;
+}
+
+//email validation
+
+const emailPattern = /^[a-zA-Z0.-9_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+if (emailPattern.test(email)) {
+  console.log("Email Matched!");
+  isValid = true;
+}
+
+//password validation
+
+const passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/;
+if (passwordPattern.test(password)) {
+  console.log("password is valid");
+}
+
+if (password !== confirmPassword) {
+  console.log("Password doesnot match");
 }
