3f21f6d3f6ac6a44fe7df07d101d15cac6952ca5
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-25 20:13:51 +0545

    Day 2

---
+++ 30-Day-Javascript/Day_02_LeetCode.js
@@ -0,0 +1,12 @@
+const Username = "Salbeen";
+const email = "salbeen@gmail.com";
+const password = "salbeen123";
+const confirmPassword = "salbeen1234";
+const age = 20;
+
+let isValid = true;
+
+if (Username === "") {
+  console.log("Username cannot be empty");
+  isValid = false;
+}
